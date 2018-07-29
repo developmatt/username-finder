b3bda188a28939064c2e532d11544a9bda1d071f
Matheus Godoi <matheusasdev@gmail.com>
2018-07-29 19:34:00 -0300

    Verifying username availability in twitter

--- src/App.js
+++ src/App.js
@@ -11,25 +11,62 @@ class App extends Component {
 
     this.state = {
       services: {
-        facebook: {name: 'Facebook', address: 'https://facebook.com', image: 'facebook.svg', active: false},
-        twitter: {name: 'Twitter', address: 'https://twitter.svg', image: 'twitter.svg', active: false},
-        instagram: {name: 'Instagram', address: 'https://instagram.com', image: 'instagram.svg', active: false},
-        linkedin: {name: 'LinkedIn', address: 'https://linkedin.com', image: 'linkedin.svg', active: false},
-        gmail: {name: 'Gmail', address: 'https://gmail.google.com', image: 'gmail.svg', active: false},
-        outlook: {name: 'Outlook', address: 'https://outlook.com', image: 'outlook.svg', active: false},
-        github: {name: 'Github', address: 'https://github.com', image: 'github-logo.svg', active: false}
-      }
+        facebook: {name: 'Facebook', address: 'https://facebook.com', image: 'facebook.svg', active: false, available: 'unknown'},
+        twitter: {name: 'Twitter', address: 'https://twitter.com/', image: 'twitter.svg', active: true, available: 'unknown'},
+        instagram: {name: 'Instagram', address: 'https://instagram.com', image: 'instagram.svg', active: false, available: 'unknown'},
+        linkedin: {name: 'LinkedIn', address: 'https://linkedin.com', image: 'linkedin.svg', active: false, available: 'unknown'},
+        gmail: {name: 'Gmail', address: 'https://gmail.google.com', image: 'gmail.svg', active: false, available: 'unknown'},
+        outlook: {name: 'Outlook', address: 'https://outlook.com', image: 'outlook.svg', active: false, available: 'unknown'},
+        github: {name: 'Github', address: 'https://github.com/', image: 'github-logo.svg', active: false, available: 'unknown'}
+      },
+      username: ''
     }
   }
 
-  onChange = (valor) => {
+  onChange = (srv) => {
     let obj = Object.assign({}, this.state.services);
-    if(obj[valor].active){
-      obj[valor].active = false;
+    if(obj[srv].active){
+      obj[srv].active = false;
     }else{
-      obj[valor].active = true;
+      obj[srv].active = true;
     }
     this.setState({obj})
+  }
+
+  updateUsername = (unames) => {
+    this.setState({username: unames.target.value});
+  }
+
+  searchUserName = () => {
+    for(let service in this.state.services){
+      var obj = Object.assign({}, this.state.services);
+
+      if(this.state.services[service].active){
+        obj[service].available = 'waiting';
+        this.getUserNameInAddress(obj[service])
+          .then(result => console.log(result))
+          .catch(error => console.log(error))
+
+
+        //obj[service].available = 'available';
+      }
+      this.setState({obj});
+    }
+  }
+
+  getUserNameInAddress = (service) => {
+    return new Promise((resolve, reject) => {
+      var xmlHttp = new XMLHttpRequest();
+
+      xmlHttp.open( "GET", service.address + this.state.username, false );
+      xmlHttp.send( null );
+      let result = xmlHttp;
+      if(result.status === 200){
+        resolve(result);
+      }else{
+        reject('Not found');
+      }
+    })
   }
 
   render() {
@@ -45,7 +82,7 @@ class App extends Component {
         <div className="servicesList row">
           {services}
         </div>
-          <Input />
+          <Input updateUsername={this.updateUsername} username={this.state.username} searchUserName={this.searchUserName} />
         </div>
       </div>
     );
--- src/input/input.js
+++ src/input/input.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
 
 class Input extends Component {
+
+    searchUserName = (e) => {
+        e.preventDefault();
+        this.props.searchUserName();
+    }   
+
     render() {
         return (
             <div className="row mt-5">
-               <div className="col-12 form-inline">
+               <form className="col-12 form-inline">
                     <div className="form-group mr-1">
-                        <input className="form-control" type="text" placeholder="Nome" />
+                        <input className="form-control" type="text" placeholder="Nome" value={this.props.username} onChange={this.props.updateUsername}/>
                     </div>
 
                     <div className="form-group">
-                        <button className="btn btn-primary">Check</button>
+                        <button className="btn btn-primary" onClick={this.searchUserName}>Check</button>
                     </div>
-               </div>
+               </form>
             </div>
         )
     }
